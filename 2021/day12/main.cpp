276c5f539893b7a2111ec4f05e35f67116e66d4f
John Susi <john@susi.se>
2021-12-12 06:46:00 +0100

    Day 12: Passage Pathing

--- 2021/day12/main.cpp
+++ 2021/day12/main.cpp
@@ -9,79 +9,54 @@
 
 #include <common/string.hpp>
 
-struct Node {
-    std::string name;
-    std::vector<std::string> edges;
-    bool operator<(const Node& other) const { return name < other.name; }
-};
+using Node = std::vector<std::string>;
 
 auto parse(std::istream& infile)
 {
     std::map<std::string, Node> map;
     for (std::string line; std::getline(infile, line);) {
         auto [from, to] = split(line, "-");
-        map[from].edges.push_back(to);
-        map[to].edges.push_back(from);
+        map[from].push_back(to);
+        map[to].push_back(from);
     }
     return map;
 }
 
-auto part1(std::map<std::string, Node> map)
+template <typename F> auto search(const std::map<std::string, Node>& map, F&& check)
 {
-    int result = 0;
-
+    int result  = 0;
     auto fringe = std::deque{std::tuple{std::string{"start"}, std::set<std::string>{}}};
-
     while (!fringe.empty()) {
         auto [cur, visited] = fringe.back();
         fringe.pop_back();
-
         if (cur == "end") {
             ++result;
             continue;
         }
-
-        if (cur[0] >= 'a' && cur[0] <= 'z')
-            if (visited.contains(cur)) continue;
-
+        if (!check(cur, visited)) continue;
         visited.insert(cur);
-
-        for (auto edge : map[cur].edges) fringe.emplace_back(edge, visited);
+        for (auto edge : map.at(cur)) fringe.emplace_back(edge, visited);
     }
-
     return result;
+}
+
+auto part1(std::map<std::string, Node> map)
+{
+    return search(map, [](auto cur, auto& visited) {
+        return !(cur[0] >= 'a' && cur[0] <= 'z' && visited.contains(cur));
+    });
 }
 
 auto part2(std::map<std::string, Node> map)
 {
-    int result = 0;
-
-    auto fringe = std::deque{std::tuple{std::string{"start"}, std::set<std::string>{}}};
-
-    while (!fringe.empty()) {
-        auto [cur, visited] = fringe.back();
-        fringe.pop_back();
-
-        if (cur == "end") {
-            ++result;
-            continue;
-        }
-
-        if (cur == "start" && visited.contains(cur)) continue;
-
-        if (cur[0] >= 'a' && cur[0] <= 'z' && visited.contains(cur))
-        {
-            if (visited.contains("<twice>"))
-                continue;
+    return search(map, [](auto cur, auto& visited) {
+        if (cur == "start" && visited.contains(cur)) return false;
+        if (cur[0] >= 'a' && cur[0] <= 'z' && visited.contains(cur)) {
+            if (visited.contains("<twice>")) return false;
             visited.insert("<twice>");
         }
-
-        visited.insert(cur);
-
-        for (auto edge : map[cur].edges) fringe.emplace_back(edge, visited);
-    }
-
-    return result;
+        return true;
+    });
 }
 
 int main()
